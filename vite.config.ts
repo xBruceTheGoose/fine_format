5763f1e196510ce7a7bb887f1e2c0687e3a43d33
goldengoose000 <juschold827@gmail.com>
2025-06-05 06:06:30 -0400

    Fix API key configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
     define: {
-      'process.env': {
-        API_KEY: JSON.stringify(env.API_KEY)
-      }
+      'process.env.API_KEY': JSON.stringify(env.API_KEY)
     },
     resolve: {
       alias: {
